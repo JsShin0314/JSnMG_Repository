f31583b5ba5693ad32ef03bb266b965237e34cc2
Jsshin <nicesun314@gmail.com>
2019-07-01 16:40:32 +0900

    new bowling app

---
+++ app/src/main/java/bowlingapp/com/example/hr/bowlingapp/MainActivity.java
@@ -0,0 +1,13 @@
+package bowlingapp.com.example.hr.bowlingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
